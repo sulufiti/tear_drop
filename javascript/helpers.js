cd6b158578f4c453ed2e203e66e8c0a337ac03d2
Red Hendery & Sulu Fiti <pair+redhendery.sulufiti@devbootcamp.com>
2015-10-28 15:41:22 +1300

    falling asleep

--- javascript/helpers.js
+++
@@ -1,9 +0,0 @@
-function animate() {
-  var circle = document.getElementById("circle1")
-  var x = circle.getAttribute("cx")
-  var newX = 2 + parseInt(x)
-  if(newX > 500) {
-    newX = 50;
-  }
-  circle.setAttribute("cx", newX)
-}
